14c5c5441dfdbd6b25c87f3f801db826ab8796a8
DominaSif <mefisttt000@gmail.com>
2026-05-29 09:20:55 +0300

    feat: setup roles grants

---
+++ db/init/grants.sql
@@ -0,0 +1,7 @@
+--- app_user data access
+GRANT SELECT, INSERT, UPDATE ON documents TO app_user;
+GRANT SELECT ON users TO app_user;
+
+-- migration full perms
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO migrations;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO migrations;
