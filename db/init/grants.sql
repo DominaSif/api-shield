7f0d7f9fed68e63e903e55a5b145075a89e4fffb
DominaSif <mefisttt000@gmail.com>
2026-06-26 17:43:38 +0300

    fix insert permissions for app_user

--- db/init/grants.sql
+++ db/init/grants.sql
@@ -5,3 +5,5 @@ GRANT SELECT ON users TO app_user;
 -- migration full perms
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO migrations;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO migrations;
+
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO app_user;
